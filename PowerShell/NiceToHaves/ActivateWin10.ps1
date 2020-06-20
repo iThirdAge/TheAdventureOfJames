9e5d8fb9e1a84b4b9a2253aa3d8b29ff3feb633a
Harrison Vu <49425360+iThirdAge@users.noreply.github.com>
2020-06-20 10:03:22 -0400

    Update ActivateWin10.ps1

--- PowerShell/NiceToHaves/ActivateWin10.ps1
+++ PowerShell/NiceToHaves/ActivateWin10.ps1
@@ -1,3 +1,5 @@
+# Harrison Vu
+
 $computer = gc env:computername
 $key = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
 $service = get-wmiObject -query "select * from SoftwareLicensingService" -computername $computer
