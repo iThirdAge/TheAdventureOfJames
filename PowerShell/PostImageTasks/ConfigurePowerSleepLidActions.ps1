f6b9bd3b5c754f9bc4f9f93d2d7ec9cb388fcce6
Harrison Vu <49425360+iThirdAge@users.noreply.github.com>
2020-06-20 10:06:08 -0400

    Update ConfigurePowerSleepLidActions.ps1

--- PowerShell/PostImageTasks/ConfigurePowerSleepLidActions.ps1
+++ PowerShell/PostImageTasks/ConfigurePowerSleepLidActions.ps1
@@ -1,3 +1,5 @@
+# Harrison Vu
+
 # tell power button to shutdown
 powercfg /setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 3
 powercfg /setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 3
