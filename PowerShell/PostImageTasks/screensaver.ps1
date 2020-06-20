eeb67bd513b1aaf5091df223003c02d944d935ea
Harrison Vu <49425360+iThirdAge@users.noreply.github.com>
2020-06-20 10:06:41 -0400

    Update screensaver.ps1

--- PowerShell/PostImageTasks/screensaver.ps1
+++ PowerShell/PostImageTasks/screensaver.ps1
@@ -1,3 +1,5 @@
-﻿Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveActive -Value 1
+# Harrison Vu
+
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveActive -Value 1
 Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveTimeOut -Value 900
 Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name scrnsave.exe -Value "c:\windows\system32\Ribbons.scr"
